34fe96e7b2893c8a751cdf504d9c6abe2c179e45
Sebastian Moreira <3292@holbertonschool.com>
2021-06-17 10:18:11 -0700

    commit

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -9,7 +9,3 @@ int main(void)
 {
   return (0);
 }
-julien@ubuntu:~/c/0x00$ ./1-compiler
-julien@ubuntu:~/c/0x00$ ls
-0-preprocessor  1-compiler   c            main.o
-Makefile               100-intel      main.c  main.s
