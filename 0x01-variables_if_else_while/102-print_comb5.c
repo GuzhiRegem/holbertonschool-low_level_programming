294d8c6a0f180b4908eacaa44cf94165d6ad6207
Sebastian Moreira <3292@holbertonschool.com>
2021-06-18 09:57:44 -0700

    commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - print a message with printf
+ *
+ *Return: end program
+ */
+int main(void)
+{
+	int i, i_;
+
+	for (i = 0; i <= 98; i++)
+	{
+		for (i_ = i + 1; i_ <= 99; i_++)
+		{
+			putchar((i / 10)+'0');
+			putchar((i % 10)+'0');
+			putchar(' ');
+			putchar((i_ / 10)+'0');
+			putchar((i_ % 10)+'0');
+			if (i < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
