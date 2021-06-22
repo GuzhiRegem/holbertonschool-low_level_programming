1d31bac94d01c43bbd401016e3181db4ea966c57
Sebastian Moreira <3292@holbertonschool.com>
2021-06-22 09:00:43 -0700

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *print_sign - print a message with printf
+ *@n: The character to print
+ *
+ *Return: end program
+ */
+int print_sign(int n)
+{
+	return ((n > 0) - (n < 0));
+}
