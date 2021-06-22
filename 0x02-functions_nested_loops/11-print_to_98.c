69c65e821635ad3538fba165d23408d018ff8566
Sebastian Moreira <3292@holbertonschool.com>
2021-06-22 10:41:47 -0700

    commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ *putnum - print a message with printf
+ *@n: The character to print
+ *
+ *Return: end program
+ */
+void putnum(int n)
+{
+	if (n > 99)
+	{
+		_putchar((n / 100) + '0');
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n > 9)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
+}
+
+/**
+ *print_to_98 - print a message with printf
+ *@n: The character to print
+ *
+ *Return: end program
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	i = n + 0;
+        while (i != 98)
+	{
+		putnum(i);
+		_putchar(',');
+		_putchar(' ');
+		if i > 98
+			i--;
+		else
+			i++;
+	}
+	putnum(98);
+	_putchar('\n');
+}
