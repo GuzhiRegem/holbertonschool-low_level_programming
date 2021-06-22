5b1852a7a0897fafb2ebadfc8df59fa7bb3f4036
Sebastian Moreira <3292@holbertonschool.com>
2021-06-22 08:11:10 -0700

    commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include "holberton.h"
 
-/**
- *main - print a message with printf
- *
- *Return: end program
- */
 int main(void)
 {
-        printf("Holberton\n");
-	return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return(0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *main - print a message with printf
+ *
+ *Return: end program
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
