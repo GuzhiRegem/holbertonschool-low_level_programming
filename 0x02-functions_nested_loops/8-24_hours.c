9e37e1c52c77066fe9709b9b1883ab4f2d1ffa77
Sebastian Moreira <3292@holbertonschool.com>
2021-06-22 09:40:39 -0700

    commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *jack_bauer - print a message with printf
+ *
+ *Return: end program
+ */
+void jack_bauer(void)
+{
+	int i, _i;
+
+	i = 0;
+	while (i < 24)
+	{
+		for (_i = 0; _i < 60; _i++)
+		{
+			_putchar(i / 10);
+			_putchar(i % 10);
+			_putchar(':');
+			_putchar(_i / 10);
+			_putchar(_i % 10);
+			_putchar('\n');
+		}
+		i++;
+	}
+}
