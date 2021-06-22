05f5f69e1da8ee5adee304f534237133d445b413
Sebastian Moreira <3292@holbertonschool.com>
2021-06-22 08:45:04 -0700

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,11 @@
 
 /**
  *_islower - print a message with printf
+ *@c: The character to print
  *
  *Return: end program
  */
 int _islower(int c)
 {
-	return (((c >= 'a') && (c <= 'a')));
+	return (((c >= 'a') && (c <= 'z')));
 }
