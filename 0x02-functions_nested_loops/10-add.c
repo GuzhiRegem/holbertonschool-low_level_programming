9e0ed914da312ea16f8811b24a7758d25870b5e6
Sebastian Moreira <3292@holbertonschool.com>
2021-06-22 10:19:31 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *add - print a message with printf
+ *@a: The character to print
+ *@b: The character to print
+ *
+ *Return: end program
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
