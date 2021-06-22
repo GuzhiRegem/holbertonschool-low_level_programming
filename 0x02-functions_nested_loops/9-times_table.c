145aedeed8e40924bd40e615e9e9c2b8657ab50e
Sebastian Moreira <3292@holbertonschool.com>
2021-06-22 10:05:21 -0700

    commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ *jack_bauer - print a message with printf
+ *
+ *Return: end program
+ */
+void jack_bauer(void)
+{
+	int i, _i, r;
+
+	i = 0;
+	while (i < 10)
+	{
+		_i = 0;
+		while (_i < 10)
+		{
+			r = i * _i;
+			if (r < 10)
+			{
+				_putchar(r + '0');
+			}
+			else
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+			if (_i < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+		i++;
+	}
+}
