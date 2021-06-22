cfb8e4bf1b5f1d11298b67288b820bd0c0d51c03
Sebastian Moreira <3292@holbertonschool.com>
2021-06-22 09:12:31 -0700

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *print_last_digit - print a message with printf
+ *@c: The character to print
+ *
+ *Return: end program
+ */
+int print_last_digit(int c)
+{
+	return ((c % 10));
+}
