41f1ce301cee8fc2e1bf75944db4203fb5a32ee9
Sebastian Moreira <3292@holbertonschool.com>
2021-06-23 12:19:46 -0700

    commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,5 +9,12 @@ int main(void)
 		if (((i % 5) == 0) || ((i % 3) == 0))
 			n += i;
 	}
+	_putchar(((n / 100000) % 10) + '0');
+	_putchar(((n / 10000) % 10) + '0');
+	_putchar(((n / 1000) % 10) + '0');
+	_putchar(((n / 100) % 10) + '0');
+	_putchar(((n / 10) % 10) + '0');
+	_putchar(((n / 1) % 10) + '0');
+	_putchar('\n');
 	return(0);
 }
