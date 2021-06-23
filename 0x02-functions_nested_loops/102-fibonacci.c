98949309ff9b22b4326cf8c9588fc64944abdd57
Sebastian Moreira <3292@holbertonschool.com>
2021-06-23 12:36:39 -0700

    commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - print a message with printf
+ *
+ *Return: end program
+ */
+int main(void)
+{
+	long int a, b, c;
+	int i;
+
+	a = 1;
+	b = 2;
+	c = 0;
+	printf("1, 2, ");
+	for (i = 0; i < 47; i++)
+	{
+		c = a + b;
+		printf("%ld, ", c);
+		a = b;
+		b = c;
+	}
+	c = a + b;
+	printf("%ld,", c);
+	return (0);
+}
