91fd660463da4b0f4b03035856af68da5424d11a
Sebastian Moreira <3292@holbertonschool.com>
2021-06-23 11:34:25 -0700

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,9 +5,9 @@ void print_times_table(int n)
 	int a, b, c;
 	if ((n >= 0) && (n <= 15))
 	{
-		for(a = 0; a < n; a++)
+		for(a = 0; a <= n; a++)
 		{
-			for(b = 0; b < n; b++)
+			for(b = 0; b <= n; b++)
 			{
 				c = a * b;
 				if (b > 0)
