1c9d30139bf50855b1361f6127910cf4f3260aa7
Sebastian Moreira <3292@holbertonschool.com>
2021-06-24 09:06:50 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,28 +8,25 @@
 int main(void)
 {
 	int i, a, b;
-	for(i = 1; i <= 100; i++)
+
+	for (i = 1; i <= 100; i++)
 	{
 		a = 1;
 		b = (i < 100);
 		if ((i % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 			a = 0;
 		}
 		if ((i % 5) == 0)
 		{
 			printf("Buzz");
 			a = 0;
-			if (b)
-				printf(" ");
 		}
 		if (a)
-		{
 			printf("%d", i);
-			if (b)
-				printf(" ");
-		}
+		if (b)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
