6885024e0d4308720017a74545e5f755ab359de2
Sebastian Moreira <3292@holbertonschool.com>
2021-06-29 08:18:51 -0700

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,21 +6,27 @@
  */
 int _atoi(char *s)
 {
-	int sign, number, c, after;
+	int sign, number, c, after, _after;
 
 	sign = 1;
 	number = 0;
 	after = 1;
+	_after = 1;
 	for (c = 0; s[c]; c++)
 	{
-		if ((s[c] >= '0') && (s[c] <= '9'))
+		if (_after)
 		{
-			after = 0;
-			number *= 10;
-			number += (s[c] - '0');
+			if ((s[c] >= '0') && (s[c] <= '9'))
+			{
+				after = 0;
+				number *= 10;
+				number += (s[c] - '0');
+			}
+			else if (after && (s[c] == '-'))
+				sign *= -1;
+			else if (s[c] != ' ')
+				_after = 0;
 		}
-		else if (after && (s[c] == '-'))
-			sign *= -1;
 	}
 	return (sign * number);
 }
