0c9e3e74533bd2f4b654616f97f6a77ac3d93220
Sebastian Moreira <3292@holbertonschool.com>
2021-06-29 09:21:50 -0700

    commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,36 +1,13 @@
-/**
- *_atoi - coso
- *@s : a
- *
- *Return: int
- */
-int _atoi(char *s)
-{
-	double number;
-	int sign, c, after, _after, out;
+#include <stdio.h>
+#include <stdlib.h>
 
-	sign = 1;
-	number = 0;
-	after = 1;
-	_after = 1;
-	for (c = 0; s[c]; c++)
-	{
-		if (_after)
-		{
-			if ((s[c] >= '0') && (s[c] <= '9'))
-			{
-				after = 0;
-				number *= 10;
-				number += (s[c] - '0');
-			}
-		}
-		if (after && (s[c] == '-'))
-			sign *= -1;
-		if ((((s[c] >= 'A') && (s[c] <= 'Z')) ||
-		     ((s[c] >= 'a') && (s[c] <= 'z'))) && (after == 0))
-			_after = 0;
-	}
-	number *= sign;
-	out = number;
-	return (out);
+/**
+ *main - hola
+ *
+ *Return: a
+ */
+int main(void)
+{
+	printf("hola");
+	return (0);
 }
