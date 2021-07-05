97b586f0d7e05098b057f42339534af6ed392523
Sebastian Moreira <3292@holbertonschool.com>
2021-07-05 10:12:28 -0700

    commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,9 +12,9 @@ unsigned int _strspn(char *s, char *accept)
 	int a, b, out;
 
 	out = 0;
-	for (a = 0; *(s + a) == 0; a++)
+	for (a = 0; s[a]; a++)
 	{
-	        for (b = 0; *(accept + b) == 0; b++)
+	        for (b = 0; accept[b]; b++)
 		{
 			if (*(s + a) == *(accept + b))
 				out++;
