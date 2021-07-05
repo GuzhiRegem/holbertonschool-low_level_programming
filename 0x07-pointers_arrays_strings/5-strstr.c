331ffb4240fb01cb2db5194273637fe2db9231fe
Sebastian Moreira <3292@holbertonschool.com>
2021-07-05 11:08:21 -0700

    commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,27 +7,27 @@
  *
  *Return: end program
  */
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
 {
 	int a, b, c, checking, found;
 	char *final;
 
 	checking = 0;
 	found = 0;
-	for (a = 0; *(haystack + a) == 0; a++)
+	for (a = 0; haystack[a]; a++)
 	{
 	        if (checking == 0)
 		{
-			if (*(haystack + a) == *(needle))
+			if (haystack[a] == *(needle))
 				checking = 1;
 			c = a;
 		}
 		else
 		{
 			b = a - c;
-			if (*(needle + b) != *(haystack + a))
+			if (needle[b] != haystack[a])
 				checking = 0;
-			else if (*(needle + b + 1) == 0)
+			else if (needle[b + 1] == 0)
 			{
 				found = 1;
 				break;
