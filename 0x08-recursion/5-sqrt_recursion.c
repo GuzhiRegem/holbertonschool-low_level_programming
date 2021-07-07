2f8bbf87760d1306faade658df8f0f48de6d03a4
Sebastian Moreira <3292@holbertonschool.com>
2021-07-07 11:09:47 -0700

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,21 @@
 #include "holberton.h"
 
 /**
+ *comp - print a message with printf
+ *@n : number
+ *@y : number
+ *
+ *Return: end program
+ */
+int comp(int n, int y)
+{
+	if (y * y == n)
+		return(y);
+	else if (y * y > n)
+		return(-1);
+	return (comp(n, y + 1));
+}
+/**
  *_sqrt_recursion - print a message with printf
  *@n : number
  *
@@ -8,9 +23,5 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (n == 1)
-		return (1);
-	return (_sqrt_recursion(n - 1) / 2);
+	return (comp(n, 1))
 }
