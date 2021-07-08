c99624bb59a897ce6aca3d4a304c0f794ccd005b
Sebastian Moreira <3292@holbertonschool.com>
2021-07-08 07:00:03 -0700

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,24 @@
 #include "holberton.h"
 
 /**
+ *func - print a message with printf
+ *@n : number
+ *@i : num
+ *
+ *Return: end program
+ */
+int func(int n, int i)
+{
+        if (i == 1)
+	{
+		return (1);
+	}
+	else if((n % i) == 0)
+		return(0);
+	else
+		return(func(n, i - 1));
+}
+/**
  *is_prime_number - print a message with printf
  *@n : number
  *
@@ -8,12 +26,10 @@
  */
 int is_prime_number(int n)
 {
-	int i, is_prime = 0;
-
-	for (i = 1; i < (n / 2); i++)
-	{
-		if ((n % i) == 0)
-			is_prime = 1;
-	}
-	return (is_prime);
+	if (n > 1)
+		return (func(n, n - 1));
+	else if (n < 0)
+		return (-1);
+	else
+		return(n);
 }
