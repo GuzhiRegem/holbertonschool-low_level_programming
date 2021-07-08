942b1670b6d39bd89f54f19b8ce32a98b8e1a4eb
Sebastian Moreira <3292@holbertonschool.com>
2021-07-08 07:07:41 -0700

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -26,9 +26,11 @@ int func(int n, int i)
  */
 int is_prime_number(int n)
 {
-	if (n > 0)
+	if (n > 1)
 		return (func(n, n - 1));
 	else if (n < 0)
+		return (0);
+	else if (n == 0)
 		return (0);
 	else
 		return (1);
