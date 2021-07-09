ad6ec15526d817a213d19205c4cedba474ddaa3f
Sebastian Moreira <3292@holbertonschool.com>
2021-07-09 07:27:45 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - func
+ *@argc : ja
+ *@argv :ja
+ *
+ *Return: a
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - func
+ *@argc : ja
+ *@argv :ja
+ *
+ *Return: a
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
