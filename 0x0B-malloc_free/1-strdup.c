f680010b0e815eb8d9f5db0263a19418cb47384a
Sebastian Moreira <3292@holbertonschool.com>
2021-07-13 08:14:52 -0700

    commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *_strdup - a
+ *@str : a
+ *
+ *Return: la
+ */
+char *_strdup(char *str);
+{
+	char *p = NULL;
+	unsigned int i, a;
+
+	for(a = 0; str[a]; a++)
+		continue;
+	a++;
+	if (size > 0)
+	{
+		p = malloc(a);
+		if (p != NULL)
+		{
+			for (i = 0; i < a; i++)
+				p[i] = str[i];
+		}
+	}
+	return (p);
+}
