c0eae16604a5260474cc43a48cedb2fdfd7dea85
Sebastian Moreira <3292@holbertonschool.com>
2021-07-13 09:03:46 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,28 +13,28 @@ char *str_concat(char *s1, char *s2)
 	char *p = NULL;
 	unsigned int size1, size2 = 0;
 
-	if (s1)
+	if (s1 != NULL)
 	{
 		for (; s1[size1]; size1++)
 			continue;
 	}
-	if (s2)
+	if (s2 != NULL)
 	{
 		for (; s2[size2]; size2++)
 			continue;
 	}
 	size2++;
 	p = malloc(size1 + size2);
-	if (p)
+	if (p != NULL)
 	{
 		size1 = 0;
 		size2 = 0;
-		if (s1)
+		if (s1 != NULL)
 		{
 			for (; s1[size1]; size1++)
 				p[size1] = s1[size1];
 		}
-		if (s2)
+		if (s2 != NULL)
 		{
 			for (; s2[size2]; size2++)
 				p[size1 + size2] = s2[size2];
