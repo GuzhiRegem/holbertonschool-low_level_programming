e5b9ca56314db43a2e597800c384017e53d9961c
Sebastian Moreira <3292@holbertonschool.com>
2021-07-13 09:35:05 -0700

    commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *alloc_grid - a
+ *@width : a
+ *@height : b
+ *
+ *Return: la
+ */
+int **alloc_grid(int width, int height)
+{
+	char *p = NULL;
+	unsigned int i;
+
+	if ((width > 0) && (height > 0))
+	{
+		p = malloc(width * height);
+		if (p)
+		{
+			for (i = 0; i < (width * height); i++)
+				p[i] = 0;
+		}
+	}
+	return (p);
+}
