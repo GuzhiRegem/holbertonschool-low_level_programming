667eb737d2d9b3448a7eb271287dff9e59dc8a96
Sebastian Moreira <3292@holbertonschool.com>
2021-07-13 09:00:50 -0700

    commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *str_concat - a
+ *@s1 : a
+ *@s2 : a
+ *
+ *Return: la
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p = NULL;
+	unsigned int size1, size2 = 0;
+
+	if (s1)
+	{
+		for (; s1[size1]; size1++)
+			continue;
+	}
+	if (s2)
+	{
+		for (; s2[size2]; size2++)
+			continue;
+	}
+	size2++;
+	p = malloc(size1 + size2);
+	if (p)
+	{
+		size1 = 0;
+		size2 = 0;
+		if (s1)
+		{
+			for (; s1[size1]; size1++)
+				p[size1] = s1[size1];
+		}
+		if (s2)
+		{
+			for (; s2[size2]; size2++)
+				p[size1 + size2] = s2[size2];
+		}
+		p[size1 + size2] = 0;
+	}
+	return (p);
+}
