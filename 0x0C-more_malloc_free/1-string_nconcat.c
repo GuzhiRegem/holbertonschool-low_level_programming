b1cba46fa699aa60b5bfcbe5d52c6e3145057334
Sebastian Moreira <3292@holbertonschool.com>
2021-07-15 09:38:59 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,12 +12,18 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p = NULL;
-	unsigned int len1, len2, i1, i2 = 0;
+	unsigned int len1, len2, i1, i2;
 
 	if (s1)
-		len1 = sizeof(s1) +2;
+	{
+		for (len1 = 0; s1[len1]; len1++)
+			continue;
+	}
 	if (s2)
-		len2 = sizeof(s2);
+	{
+		for (len2 = 0; s2[len2]; len2++)
+			continue;
+	}
 	if (n < len2)
 		len2 = n;
 	p = malloc(len1 + len2);
