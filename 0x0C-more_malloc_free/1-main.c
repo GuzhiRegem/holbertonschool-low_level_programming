87d76a28da4b42ef8b2ef747671e7e8e9b9e78c0
Sebastian Moreira <3292@holbertonschool.com>
2021-07-15 10:13:18 -0700

    commit

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char *concat;
 
-	concat = string_nconcat("", "5678", 2);
+	concat = string_nconcat(NULL, "5678", 2);
 	printf("%s\n", concat);
 	free(concat);
 	return (0);
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,13 +13,15 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p = NULL;
-	unsigned int len1, len2, i1, i2 = 0;
+	unsigned int len1, len2, i1, i2;
 
+	len1 = 0;
 	if (s1)
 	{
 		for (len1 = 0; s1[len1]; len1++)
 			continue;
 	}
+	len2 = 0;
 	if (s2)
 	{
 		for (len2 = 0; s2[len2]; len2++)
@@ -28,6 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < len2)
 		len2 = n;
 	p = malloc(len1 + len2 + 1);
+	printf("%u\n",(len1 + len2 + 1));
 	if (p)
 	{
 		for (i1 = 0; (i1 < len1) && s1; i1++)
