041d5bee3787a9949437f94fda928c3900602c3d
Sebastian Moreira <3292@holbertonschool.com>
2021-07-19 12:03:50 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-struct dog {
+/**
+ *struct dog - a
+ *@name: a
+ *@age: a
+ *@owner: a
+ *
+ *Description: a
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
