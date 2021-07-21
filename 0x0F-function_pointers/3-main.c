42d1d37657f251f863bbb6cfe7902f3ea32be87d
Sebastian <3292@holbertonschool.com>
2021-07-21 14:17:17 -0700

    commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,14 +12,14 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2;
-        int (*func)(int, int);
+	int (*func)(int, int);
 
 	if (!(argc == 4))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	func= get_op_func(argv[2]);
+	func = get_op_func(argv[2]);
 	if (!func)
 	{
 		printf("Error\n");
@@ -27,8 +27,7 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if (((argv[2][1] == 0) && ((argv[2][0] == '%') || (argv[2][0] == '/')))
-	    && (num2 == 0))
+	if ((argv[2][0] == '%') || (argv[2][0] == '/')) && (num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
