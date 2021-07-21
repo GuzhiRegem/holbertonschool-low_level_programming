21c4d4e99ae0ea1a86f19cb50895f75ab42750c8
Sebastian <3292@holbertonschool.com>
2021-07-21 08:56:53 -0700

    commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 /**
  *array_iterator - a
  *@array: a
@@ -8,7 +10,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	long unsigned int i;
 
 	for (i = 0; i < size; i++)
 		action(array[i]);
