cdd6694bcd7ea8335479d172d898e94a1fc9175f
Sebastian <3292@holbertonschool.com>
2021-07-21 14:04:06 -0700

    commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ *main - a
+ *@argc: a
+ *@argv: a
+ *
+ *Return: a
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+        int (*func)(int, int);
+
+	if (!(argc == 4))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	func= get_op_func(argv[2]);
+	if (!func)
+	{
+		printf("Error 2\n");
+		exit(99);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (((argv[2][1] == 0) && ((argv[2][0] == '%') || (argv[2][0] == '/')))
+	    && (num2 == 0))
+	{
+		printf("Error 3\n");
+		exit(100);
+	}
+	printf("%d\n", func(num1, num2));
+	return (0);
+}
