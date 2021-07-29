39c01c9c0710c43b2696094aa8cbe56276f69ef5
Sebastian <3292@holbertonschool.com>
2021-07-29 09:49:03 -0700

    commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *_str_len - a
+ *@str: a
+ *Return: a
+ */
+unsigned int _str_len(char *str)
+{
+	if (!str[0])
+		return (0);
+	return (_str_len(str + 1) + 1);
+}
+/**
+ *add_node - a
+ *@head: j
+ *@str: a
+ *Return: a
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr = NULL;
+	char *cpy;
+
+	cpy = strdup(str);
+	ptr = malloc(sizeof(list_t));
+	if (ptr && cpy)
+	{
+		ptr->str = cpy;
+		ptr->len = _str_len(cpy);
+		ptr->next = *head;
+		*head = ptr;
+	}
+	return (ptr);
+}
