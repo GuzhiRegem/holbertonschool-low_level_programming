ee5090f240c73fb460130a7d995ae7a4710433e9
Sebastian <3292@holbertonschool.com>
2021-07-29 09:26:18 -0700

    commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,18 +1,22 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *print_list - a
  *@h: j
  *Return: a
  */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
+	size_t out = 0;
 	if (h)
 	{
 		if (h->str)
 			printf("[%d] %s\n", h->len, h->str);
 		else
-			printf("[0] (nil)");
-		print_list(h->next);
+			printf("[0] (nil)\n");
+		return (print_list(h->next) + 1);
 	}
+	else
+		return (out);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
