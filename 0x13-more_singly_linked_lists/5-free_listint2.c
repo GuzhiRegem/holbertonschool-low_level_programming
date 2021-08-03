023c6643ad9bdf4fb24ae41eb0bb4021a4ac0d98
Sebastian <3292@holbertonschool.com>
2021-08-03 09:56:06 -0700

    commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -24,6 +24,9 @@ void free_listint(listint_t *head)
  */
 void free_listint2(listint_t **head)
 {
-	free_listint(*head);
-	*head = NULL;
+	if (head)
+	{
+		free_listint(*head);
+		*head = NULL;
+	}
 }
