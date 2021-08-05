549f4e7adf1fe51f086b511395d1262065754799
Sebastian <3292@holbertonschool.com>
2021-08-05 10:16:01 -0700

    commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,18 @@
 #include "main.h"
 /**
- *- a
- *@: a
+ *set_bit- a
+ *@n: a
+ *@index: a
  *Return: a
  */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 0; i < index; i++)
+		num *= 2;
+	*n = *n | num;
+	return (1);
+}
