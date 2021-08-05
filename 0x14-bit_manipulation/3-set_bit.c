57d9b572da80151b517db50070c24ef8ae6642ff
Sebastian <3292@holbertonschool.com>
2021-08-05 11:37:27 -0700

    commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int num;
 	unsigned int i;
 
-	if (!n)
+	if (index >= 64)
 		return (-1);
 	num = 1;
 	for (i = 0; i < index; i++)
