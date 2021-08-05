811bd11a7cf6b3b1c8857a588f35d901f61851db
Sebastian <3292@holbertonschool.com>
2021-08-05 09:07:22 -0700

    commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_binary - a
+ *@n: a
+ *Return: a
+ */
+void print_binary(unsigned long int n)
+{
+	if (n < 2)
+		_putchar(n + '0');
+	else
+        {
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+}
