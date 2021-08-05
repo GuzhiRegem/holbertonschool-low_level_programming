329b2ecaf41ad42876a963e41de2910604ed3c28
Sebastian <3292@holbertonschool.com>
2021-08-05 10:35:31 -0700

    commit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,21 @@
 #include "main.h"
 /**
- *- a
- *@: a
+ *flip_bits- a
+ *@n: a
+ *@m: a
  *Return: a
  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int num;
+	unsigned int out;
+
+	num = n ^ m;
+	out = 0;
+	while (num)
+	{
+		out += num & 1;
+		num / 2;
+	}
+	return (out);
+}
