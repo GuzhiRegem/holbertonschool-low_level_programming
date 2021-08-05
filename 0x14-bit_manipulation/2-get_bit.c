b3ebdd27fe5327f3cd0a0ec197533e31f2f375cc
Sebastian <3292@holbertonschool.com>
2021-08-05 11:35:00 -0700

    commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (!index)
 		return (n & 1);
-	if (!n)
+	if (!n || (index >= 64))
 		return (-1);
 	return (get_bit(n >> 1, index - 1));
 }
