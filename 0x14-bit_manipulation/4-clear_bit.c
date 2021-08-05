0b890ec7729c55d3fd75cb73e908c080b29fc77f
Sebastian <3292@holbertonschool.com>
2021-08-05 10:29:00 -0700

    commit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,6 +1,18 @@
 #include "main.h"
 /**
- *- a
- *@: a
+ *clear_bit- a
+ *@n: a
+ *@index: a
  *Return: a
  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 0; i < index; i++)
+		num *= 2;
+	*n = *n & ~num;
+	return (1);
+}
