86609c366efeb45e670e1292d12a9fa0e93a7a22
Sebastian <3292@holbertonschool.com>
2021-08-09 09:22:26 -0700

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,9 +16,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 	ssize_t printed;
 
-	fd = open(filename, O_RDONLY | O_CREAT);
 	if (!filename)
 		return (0);
+	fd = open(filename, O_RDONLY | O_CREAT);
 	if (fd == -1)
 		return (0);
 	buf = malloc(letters);
