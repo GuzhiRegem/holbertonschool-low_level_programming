2a0b07b67fc8cfaa6f6dc93944b66d6be61411e3
Sebastian <3292@holbertonschool.com>
2021-08-09 09:32:29 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, chars, _chars;
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
-	if ((fd == -1) | !filename)
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 	chars = strlen(text_content);
 	_chars = write(fd, text_content, chars);
