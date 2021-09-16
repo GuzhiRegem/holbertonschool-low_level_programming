8156a085718f4fe102a731b727e634292a189a19
Guzhi <3292@holbertonschool.com>
2021-09-16 16:47:43 -0700

    commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	next = *h;
 	while (idx && next)
 	{
+		prev = next;
 		next = next->next;
-		prev = next->prev;
 		idx--;
 	}
 	if (idx && !next)
