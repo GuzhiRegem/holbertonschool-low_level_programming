e447b44771974ae6d44d0dc70d9722ca3710bd32
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-10-07 10:35:12 -0700

    commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,23 +1,84 @@
 #include "hash_tables.h"
 
 /**
+ * new_node - creates a node
+ * @value: the value
+ * @key: the key
+ * Return: pointer to new node
+ */
+hash_node_t *new_node(const char *value, const char *key)
+{
+	hash_node_t *out = NULL;
+	char *n_key = NULL, *n_value = NULL;
+
+	out = malloc(sizeof(hash_node_t));
+	if (!out)
+		return (out);
+	n_key = malloc(strlen(key));
+	if (!n_key)
+	{
+		free(out);
+		return (NULL);
+	}
+	n_value = malloc(strlen(value));
+	if (!n_value)
+	{
+		free(out);
+		free(n_key);
+		return (NULL);
+	}
+	strcpy(n_key, key);
+	strcpy(n_value, value);
+	out->key = n_key;
+	out->value = n_value;
+	out->next = NULL;
+	return (out);
+}
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table
  * @key: the key, it cannot be an empty string
  * @value: the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char *content;
+	int exist = 0;
 	hash_node_t *ptr, *new;
+	char *val_cpy;
+	unsigned long int ind;
 
 	if (!ht || !key)
 		return (0);
-
-	ptr = ht->array[key_index(key, ht->size)];
-	
-	content = malloc(strlen(value));
-	if (!content)
+	ind = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[ind];
+	if (ptr)
+		while (ptr->next)
+		{
+			if (strcmp(key, ptr->key) == 0)
+			{
+				exist = 1;
+				break;
+			}
+			ptr = ptr->next;
+		}
+	if (exist)
+	{
+		val_cpy = malloc(strlen(value));
+		if (!val_cpy)
+			return (0);
+		strcpy(val_cpy, value);
+		if (ptr->value)
+			free(ptr->value);
+		ptr->value = val_cpy;
+		return (1);
+	}
+	new = new_node(value, key);
+	if (!new)
 		return (0);
-	strcpy(content, value);
+	if (ptr)
+		ptr->next = new;
+	else
+		ht->array[ind] = new;
+	return (1);
 }
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table you want to look into
+ * @key: key you are looking for
+ * Return: value associated with the element
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int ind, exist = 0;
+	hash_node_t *ptr;
+
+	if (!ht || !key)
+		return (NULL);
+	ind = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[ind];
+	if (!ptr)
+	{
+		printf("no existe ptr\n");
+		return (NULL);
+	}
+	while (ptr->next)
+	{
+		if (strcmp(key, ptr->key) == 0)
+		{
+			exist = 1;
+			break;
+		}
+		ptr = ptr->next;
+	}
+	if (!exist)
+		return (NULL);
+	return (ptr->value);
+}
