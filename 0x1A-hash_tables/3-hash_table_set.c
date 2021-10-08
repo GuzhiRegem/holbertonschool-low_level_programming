cc5d132c33bffe6434c1731c22b0d28e00a52595
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-10-08 06:35:11 -0700

    commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -48,7 +48,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *val_cpy = NULL;
 	unsigned long int ind;
 
-	if (!ht || !key)
+	if (!ht || !key || !value)
+		return (0);
+	if (strlen(key) == 0)
 		return (0);
 	ind = key_index((const unsigned char *)key, ht->size);
 	ptr = ht->array[ind];
--- 0x1A-hash_tables/test3.c
+++ 0x1A-hash_tables/test3.c
@@ -6,15 +6,38 @@ int main(void)
 	unsigned long int i;
 	hash_node_t *ptr;
 
-	ht = hash_table_create(12);
+	ht = hash_table_create(1024);
 	printf("created ht at: %p with size: %lu\n",(void *)ht,ht->size);
+	hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_set(ht, "c", "isfun");	
 	hash_table_set(ht, "betty", "cool");
 	hash_table_set(ht, "betty", NULL);
+	hash_table_set(ht, "c", "fun");
+	hash_table_set(ht, "hetairas", "awesome");
+	hash_table_set(ht, "mentioner", "and Kris love asm");
+	hash_table_set(ht, "heliotropes", "queens");
+	hash_table_set(ht, "neurospora", "Obelix");
+	hash_table_set(ht, "depravement", "Cool");
+	hash_table_set(ht, "serafins", "Battery Street");
+	hash_table_set(ht, "c", "isfun");
 	for (i = 0; i < ht->size; i++)
 	{
 		ptr = ht->array[i];
-		if (ptr)
-			printf("<%lu> %s : %s\n", i, ptr->key, ptr->value);
+		if (!ptr)
+			continue;
+		printf("<%lu> ", i);
+		while (ptr)
+		{
+			printf("<%s : %s> -> ", ptr->key, ptr->value);
+			ptr = ptr->next;
+		}
+		printf("\n");
 	}
 	return (EXIT_SUCCESS);
 }
