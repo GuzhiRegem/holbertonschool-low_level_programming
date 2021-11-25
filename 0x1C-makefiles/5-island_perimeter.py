785b31aa2d74a145d3cedd2627d924a12d1b5a37
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-11-25 10:38:14 -0800

    commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,16 +8,12 @@
 
 def island_perimeter(grid):
     """ island perimeter """
-    if grid is None or type(grid) is not list or type(grid[0]) is not list:
-        return 0
     perimeter = 0
-    width = len(grid[0])
-    height = len(grid)
-    for y in range(height):
-        for x in range(width):
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
             if (grid[y][x] == 1):
                 perimeter += int(grid[y + 1][x])
                 perimeter += int(grid[y - 1][x])
                 perimeter += int(grid[y][x + 1])
                 perimeter += int(grid[y][x - 1])
-    return (perimeter)
+    return perimeter
