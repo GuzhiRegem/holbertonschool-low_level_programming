a8682fe5abc675833739d01cb9e6d4b039045eb6
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-11-25 10:46:37 -0800

    commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,8 +12,8 @@ def island_perimeter(grid):
     for y in range(len(grid)):
         for x in range(len(grid[0])):
             if (grid[y][x] == 1):
-                perimeter += int(grid[y + 1][x] == 0 or y == len(grid) - 1)
+                perimeter += int(grid[y + 1][x] == 0 or y == (len(grid) - 1))
                 perimeter += int(grid[y - 1][x] == 0 or y == 0)
-                perimeter += int(grid[y][x + 1] == 0 or x == len(grid[0]) - 1)
+                perimeter += int(grid[y][x + 1] == 0 or x == (len(grid[0]) - 1))
                 perimeter += int(grid[y][x - 1] == 0 or x == 0)
     return perimeter
