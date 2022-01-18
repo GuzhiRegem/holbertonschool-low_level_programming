938f7f255ce445d8e55a0a9a3ed513777a8e52a0
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-18 11:37:12 -0300

    commit

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,64 @@
+#include "search_algos.h"
+
+/**
+ * print_arr - prints an array
+ * @array: array 
+ * @size: size
+ */
+void print_arr(int *array, size_t size)
+{
+	size_t i;
+
+	if (!array)
+		return;
+	printf("Searching in array:");
+	for (i = 0; i < size; i++)
+	{
+		printf(" %d", array[i]);
+		if (i < (size - 1))
+			printf(",");
+	}
+	printf("\n");
+}
+/**
+ * binary_search - binary_search
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: int 
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int middle, ind, offst, arr_offst = 0;
+	size_t mid_ind;
+
+	if (!array)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	print_arr(array, size);
+	if (size == 1)
+	{
+		if (*array == value)
+			return (value);
+		return (-1);
+	}
+	mid_ind = (size/2) - (1 - (size % 2));
+	middle = array[mid_ind];
+	if (value == middle)
+		return (mid_ind);
+	if (value < middle)
+	{
+		offst = - mid_ind - (2 - (size % 2));
+		ind = binary_search(array + arr_offst, size + offst, value);
+		return (ind >= 0 ? ind + arr_offst: -1);
+	}
+	if (value > middle)
+	{
+		offst = - mid_ind - 1;
+		arr_offst = mid_ind + 1;
+		ind = binary_search(array + arr_offst, size + offst, value);
+		return (ind >= 0 ? ind + arr_offst: -1);
+	}
+	return (0);
+}
