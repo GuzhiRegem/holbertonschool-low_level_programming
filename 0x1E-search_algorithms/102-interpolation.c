885f8fe3862ba746a4b65a54474fc221dea0038c
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-18 14:09:27 -0300

    commit

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -9,5 +9,26 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	return (0);
+	int start = 0, end = (int)size - 1, pos, tmp;
+
+	tmp = ((double)(end - start) / (array[end] - array[start]));
+	pos = start + (tmp * (value - array[start]));
+	if (pos > (int)size)
+	{
+		printf("Value checked array[%d] is out of range\n", pos);
+		return(-1);
+	}
+	while (start <= end && value >= array[start] && value <= array[end])
+	{
+		tmp = ((double)(end - start) / (array[end] - array[start]));
+		pos = start + (tmp * (value - array[start]));
+		printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		if (value > array[pos])
+			start = pos + 1;
+		else
+			end = pos - 1;
+	}
+	return (-1);
 }
