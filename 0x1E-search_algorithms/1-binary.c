b15cd9b8c00c27e02b4c662eccba1c81e899e610
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-19 15:00:17 -0300

    commit

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -32,9 +32,7 @@ int binary_search(int *array, size_t size, int value)
 	int middle, ind, offst, arr_offst = 0;
 	size_t mid_ind;
 
-	if (!array)
-		return (-1);
-	if (size == 0)
+	if (!array || size == 0)
 		return (-1);
 	print_arr(array, size);
 	if (size == 1)
@@ -43,22 +41,17 @@ int binary_search(int *array, size_t size, int value)
 			return (0);
 		return (-1);
 	}
-	mid_ind = (size / 2) - (1 - (size % 2));
+	mid_ind = (size / 2) - !(size % 2);
 	middle = array[mid_ind];
 	if (value == middle)
 		return (mid_ind);
-	if (value < middle)
-	{
-		offst = -mid_ind - (2 - (size % 2));
-		ind = binary_search(array + arr_offst, size + offst, value);
-		return (ind >= 0 ? ind + arr_offst : -1);
-	}
+	offst = -(size - mid_ind);
 	if (value > middle)
 	{
 		offst = -mid_ind - 1;
 		arr_offst = mid_ind + 1;
-		ind = binary_search(array + arr_offst, size + offst, value);
-		return (ind >= 0 ? ind + arr_offst : -1);
 	}
+	ind = binary_search(array + arr_offst, size + offst, value);
+	return (ind >= 0 ? ind + arr_offst : -1);
 	return (0);
 }
