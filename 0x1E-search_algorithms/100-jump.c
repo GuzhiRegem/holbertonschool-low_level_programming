019755765ddc06d4b891990b3b40f85f32acf3d3
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-19 14:25:58 -0300

    commit

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -9,21 +9,19 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int a = 0, b = sqrt(size);
+	int a = 0, jump = sqrt(size), b = 0;
 
 	if (!array)
 		return (-1);
-	while (b < (int)size)
+	while (b <= (int)size)
 	{
+		a = b;
+		b += jump;
 		printf("Value checked array[%d] = [%d]\n", a, array[a]);
 		if (array[a] == value)
 			return (a);
 		if (b >= value)
 			break;
-		if (b > (int)size)
-			return (-1);
-		a = b;
-		b += sqrt(size);
 	}
 	printf("Value found between indexes [%d] and [%d]\n", a, b);
 	for (; a <= min(b, (int)size - 1); a++)
